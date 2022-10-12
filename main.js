756b1538a4fd13ae969969e8c9479d766b761ed5
Mehmet Mert Denizman <amorylorch@gmail.com>
2022-10-12 23:28:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+      if (err) throw err;
+      res.end(content);
+
+
+    });
+  }
+});
+
+
+
+const PORT = process.env.PORT || 5500;
+server.listen(PORT, () => console.log(`Server runs ${PORT}`));
